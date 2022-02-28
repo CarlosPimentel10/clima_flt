053797a766c6209f3eb3a96ceafed667f2617d7c
CarlosPimentel10 <carlos.costa.pimentel@gmail.com>
2022-02-28 15:25:38 +0100

    push the clima app complete project

---
+++ android/app/src/main/kotlin/co/appbrewery/clima_flt/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.appbrewery.clima_flt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/networking.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper {
+  final String url;
+
+  NetworkHelper(this.url);
+
+  Future getData() async {
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+      return decodedData;
+    } else {
+      debugPrint(response.statusCode.toString());
+    }
+    
+  }
+}
